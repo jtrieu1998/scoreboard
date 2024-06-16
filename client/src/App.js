b9327b5d664e2a02a927f6876553769c4ed12f1a
jtrieu1998 <43070404+jtrieu1998@users.noreply.github.com>
2024-06-16 02:35:51 -0500

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onclick={() => {alert("ON SCOREBOARD")}}> SCOREBOARD </button>
+        <button> CONTROLLER </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
